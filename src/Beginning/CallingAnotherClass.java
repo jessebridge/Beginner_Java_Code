f4358b67ae1657431d953ce0b414c5da0447ea27
jessebridge <jesse.bridge@my.jcu.edu.au>
2017-07-27 22:05:23 +1000

    Created a class that is able to call a value from another class

--- src/Beginning/CallingAnotherClass.java
+++ src/Beginning/CallingAnotherClass.java
@@ -1,10 +1,19 @@
 package Beginning;
 
+import java.util.Scanner;
+
 public class CallingAnotherClass {
     public static void main(String[] name) {
+        String PersonName = GetName();
+        System.out.println("hi "+ PersonName);
 
+    }
 
-
+    public static String GetName() {
+        Scanner InputDevice = new Scanner(System.in);
+        System.out.println("enter your name");
+        String name = InputDevice.nextLine();
+        return name;
 
 
     }
